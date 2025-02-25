dbd11ef661b07b60443cfcc63e053c7ab36715c1
zhengtianbao <china.zhengtianbao@gmail.com>
2025-02-25 09:46:58 +0800

    add change prometheus image tag

---
+++ change_tag.sh
@@ -0,0 +1,10 @@
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/kube-state-metrics:v2.8.2 easzlab/kube-state-metrics:v2.8.2
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/kube-webhook-certgen:v1.5.1 easzlab/kube-webhook-certgen:v1.5.1
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/grafana:9.4.7 grafana/grafana:9.4.7
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/k8s-sidecar:1.22.0 quay.io/kiwigrid/k8s-sidecar:1.22.0
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/prometheus-config-reloader:v0.63.0 quay.io/prometheus-operator/prometheus-config-reloader:v0.63.0
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/prometheus-operator:v0.63.0 quay.io/prometheus-operator/prometheus-operator:v0.63.0
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/alertmanager:v0.25.0 quay.io/prometheus/alertmanager:v0.25.0
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/node-exporter:v1.5.0 quay.io/prometheus/node-exporter:v1.5.0
+docker tag registry.cn-beijing.aliyuncs.com/bogeit/prometheus:v2.42.0 quay.io/prometheus/prometheus:v2.42.0
+
